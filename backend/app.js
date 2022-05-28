84ffc3a31d816888c6c5d1c0c753910a6041de1c
amrit446 <hntr776@gmail.com>
2022-05-29 04:32:06 +0530

    first commit

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+const express=require('express');
+const mongoose=require('mongoose');
+const router=require('./routes/bookroutes')
+
+const app=express();
+
+
+// Middlewares
+app.use(express.json());
+app.use('/books', router)
+
+
+mongoose.connect('mongodb+srv://admin:admin@cluster0.mii9pbh.mongodb.net/BookApp?retryWrites=true&w=majority').then(()=>console.log("connected to the database")).then(()=>{
+    app.listen(5000);
+}).catch((err)=>console.log(err));
